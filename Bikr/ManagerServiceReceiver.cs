9b7796742746cdcfe6d9ea02c990748657cb3cdf
Jérémie Laval <jeremie.laval@gmail.com>
2014-04-21 19:30:27 -0400

    [Bikr] Add broadcast receiver to automatically launch manager service.
    This will catch both post-boot and app reinstall/update events

---
+++ Bikr/ManagerServiceReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.Content;
+using Android.App;
+
+namespace Bikr
+{
+	[BroadcastReceiver]
+	[IntentFilter (new[] { "android.intent.action.BOOT_COMPLETED", "android.intent.action.MY_PACKAGE_REPLACED" })]
+	public class ManagerServiceReceiver : BroadcastReceiver
+	{
+		public override void OnReceive (Context context, Intent intent)
+		{
+			var prefs = new PreferenceManager (context);
+			if (!prefs.FirstTimeAround && prefs.Enabled) {
+				var serviceIntent = new Intent (context, typeof(ManagerService));
+				context.StartService (serviceIntent);
+			}
+		}
+	}
+}
+
